532d42f06bba38335a2bc53f21f3c25939b07b3b
Ezequiel Vincet <ezequielautomation@gmail.com>
2026-01-28 22:28:11 -0300

    Fix prisma adapter implementation with pg pool

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,14 @@
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../generated/prisma/client';
 
+import { Pool } from 'pg';
+
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 function createPrismaClient() {
-    const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+    const connectionString = `${process.env.DATABASE_URL}`;
+    const pool = new Pool({ connectionString });
+    const adapter = new PrismaPg(pool);
     return new PrismaClient({ adapter });
 }
 
