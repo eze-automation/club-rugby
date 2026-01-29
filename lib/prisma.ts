aae15f6078560fe9834968d6a0763da1e2d24094
Ezequiel Vincet <ezequielautomation@gmail.com>
2026-01-28 22:41:37 -0300

    Revert to standard simple PrismaClient

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,18 +1,8 @@
-import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../generated/prisma/client';
-
-import { Pool } from 'pg';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-function createPrismaClient() {
-    const connectionString = `${process.env.DATABASE_URL}`;
-    const pool = new Pool({ connectionString });
-    const adapter = new PrismaPg(pool);
-    return new PrismaClient({ adapter });
-}
-
-export const prisma = globalForPrisma.prisma ?? createPrismaClient();
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') {
     globalForPrisma.prisma = prisma;
